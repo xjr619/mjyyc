f4a95a6e28b088455d19aededd83b1ce87823c0b
mapcme <1255604509@qq.com>
2018-06-09 11:35:39 +0800

    home router switch

---
+++ src/components/AddStaff.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <el-form :model="form">
+        <el-form-item label="员工姓名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RegitsterInGoods',
+  props: {
+
+  },
+  data() {
+    return {
+      form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+    @import url('../style/components/RegitsterInGoods.css');
+</style>
--- src/page/home.vue
+++ src/page/home.vue
@@ -4,19 +4,15 @@
     <el-row>
       <el-col :span="16">
         <div class="grid-content">
-          <el-button type="primary" @click="showDialog" plain>登记入货</el-button>
+          <el-button type="primary" @click="showDialog(0)" plain>登记入货</el-button>
           <router-link to="/outgoods">
             <el-button type="success" plain>出货打单</el-button>
           </router-link>
           <router-link to="/settle">
             <el-button type="info" plain>结算打单</el-button>
           </router-link>
-          <router-link to="/customer">
-            <el-button type="warning" plain>客户信息</el-button>
-          </router-link>
-          <router-link to="/staff">
-            <el-button type="danger" plain>员工信息</el-button>
-          </router-link>
+          <el-button type="warning" @click="showDialog(1)" plain>客户信息</el-button>
+          <el-button type="danger" @click="showDialog(2)" plain>员工信息</el-button>
         </div>
       </el-col>
       <el-col :span="8">
@@ -28,8 +24,11 @@
     <el-row>
       <router-view></router-view>
     </el-row>
-    <el-dialog title="登记入货" :visible.sync="dialogFormVisible" modal="true" lock-scroll="true" close-on-click-modal="false" show-close="false">
-      <regitster-in-goods></regitster-in-goods>
+    12345432
+    <el-dialog :title="titles[activeIndex]" :visible.sync="dialogFormVisible" modal="true" lock-scroll="true" close-on-click-modal="false" show-close="false">
+      <regitster-in-goods v-show="activeIndex==0"></regitster-in-goods>
+      <add-customer v-show="activeIndex==1"></add-customer>
+      <add-staff v-show="activeIndex==2"></add-staff>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
@@ -41,21 +40,29 @@
 <script>
 import Search from '../components/Search'
 import RegitsterInGoods from '../components/RegitsterInGoods'
+import AddCustomer from '../components/AddCustomer'
+import AddStaff from '../components/AddStaff'
 export default {
   data() {
     return {
-      dialogFormVisible: false
-    }
-  },
-  methods: {
-    showDialog(){
-      this.$router.push({ path: '/'})
-      this.dialogFormVisible = true
+      dialogFormVisible: false,
+      activeIndex: 0,
+      titles: ['登记入货', '新增客户', '新增员工'],
+      sonRouters: ['/', '/customer', '/staff']
     }
   },
   components: {
     Search: Search,
-    RegitsterInGoods: RegitsterInGoods
+    RegitsterInGoods: RegitsterInGoods,
+    AddCustomer: AddCustomer,
+    AddStaff: AddStaff
+  },
+  methods: {
+    showDialog(activeIndex) {
+      this.activeIndex = activeIndex
+      this.$router.push({ path: this.sonRouters[activeIndex] })
+      this.dialogFormVisible = true
+    }
   }
 }
 </script>
