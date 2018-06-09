fc5e7a2dfb25d6c26fd6dde43d88fc408faa7bdd
mapcme <1255604509@qq.com>
2018-06-09 17:15:58 +0800

    a

--- src/components/CustomerInfo.vue
+++ src/components/CustomerInfo.vue
@@ -70,6 +70,17 @@ export default {
     }
   },
   computed:{},
+  watch: {
+    '$route':function(val, oldVal){
+      if(val.query.queryString){
+        this.$set(this.tableData,this.tableData.length,{
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          })
+      }
+    }
+  },
   created() {
     
   },
